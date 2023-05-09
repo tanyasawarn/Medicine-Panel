a3a6c63cba1b7436dba40fea2787c91b7ead9d95
tanyasawarn <121800861+tanyasawarn@users.noreply.github.com>
2023-05-09 14:39:10 +0530

    Update MedicineForm.js

--- src/Components/Medicine/MedicineForm.js
+++ src/Components/Medicine/MedicineForm.js
@@ -1,22 +1,38 @@
- import classes from "./MedicineForm.module.css";
-  import {MedicineProduct} from "./Products";
-  import Product from "./Product";
+import classes from "./MedicineForm.module.css";
+import { MedicineProduct } from "./Products";
+import Product from "./Product";
 
- const MedicineForm = ()=>{
-   
-    return(
-          <section className={classes.auth}>
-             <div className={classes.control}>
-            {MedicineProduct.map((product)=>(
-                <Product data={product} />
+const MedicineForm = () => {
+  return (
+    <div>
+      <div>
+        <section className={classes.auth}>
+          <div className={classes.control}>
+            {MedicineProduct.map((product) => (
+              <Product data={product} />
             ))}
-        </div>
+          </div>
+        </section>
+        <div>
+          <section>
+            <form>
+              <div>
+                <label htmlFor="Name">Name:</label>
+                <input type="text" />
+                <label htmlFor="DESCRIPTION">Description:</label>
+                <input type="text" />
+                <label htmlFor="price">Price: Rs.</label>
+                <input type="number" />
+                <label htmlFor="qunatity">Quantiy:</label>
+                <input type="number" />
+                <button>Add</button>
+              </div>
+            </form>
           </section>
-           
-            
-           
-        
-    );
- };
+        </div>
+      </div>
+    </div>
+  );
+};
 
- export default MedicineForm;
+export default MedicineForm;
